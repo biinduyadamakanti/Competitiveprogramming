8c4d9a096fe7ffa485d23835ac78a32ac5d1815c
biinduyadamakanti <31199632+biinduyadamakanti@users.noreply.github.com>
2018-06-26 18:46:01 +0530

    Add files via upload

--- week1/day1/1.py
+++ week1/day1/1.py
@@ -1,1 +1,14 @@
-# Competitiveprogramming
+
+stock_prices=[int(x) for x in input().split(",")]
+if len(stock_prices) < 2:
+        raise ValueError('Getting a profit requires at least 2 prices')
+
+min_price  = stock_prices[0]
+max_profit = stock_prices[1] - stock_prices[0]
+for current_time in range(1, len(stock_prices)):
+    current_price = stock_prices[current_time]
+    potential_profit = current_price - min_price
+    max_profit = max(max_profit, potential_profit)
+    min_price  = min(min_price, current_price)
+
+print(max_profit)
