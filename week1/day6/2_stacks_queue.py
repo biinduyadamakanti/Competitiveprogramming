c89b64e20691c360da35abba7cd7dcd597b1682b
biinduyadamakanti <31199632+biinduyadamakanti@users.noreply.github.com>
2018-06-30 19:00:29 +0530

    Add files via upload

---
+++ week1/day6/2_stacks_queue.py
@@ -0,0 +1,106 @@
+import unittest
+
+class Stack():
+
+    def __init__(self):
+        self.stk = []
+
+    def pop(self):
+        """raises IndexError if you pop when it's empty"""
+        return self.stk.pop()
+
+    def push(self, elt):
+        self.stk.append(elt)
+
+    def is_empty(self):
+        return len(self.stk) == 0
+
+    def peek(self):
+        if not self.stk.is_empty():
+            return self.stk[-1]
+class QueueTwoStacks(object):
+    
+
+    # Implement the enqueue and dequeue methods
+    def __init__(self):
+        self.q = Stack()  # the primary queue
+        self.b = Stack()  # the reverse, opposite q (a joke: q vs b)
+        self.front = None
+
+    def is_empty(self):
+        return self.q.is_empty()
+
+    def peek(self):
+        if self.q.is_empty():
+            return None
+        else:
+            return self.front
+    def enqueue(self, elt):
+        self.front = elt
+        self.q.push(elt)
+
+    def dequeue(self):
+        """raises IndexError if you dequeue from an empty queue"""
+        while not self.q.is_empty() > 0:
+            elt = self.q.pop()
+            self.b.push(elt)
+        val = self.b.pop()
+        elt = None
+        while not self.b.is_empty() > 0:
+            elt = self.b.pop()
+            self.q.push(elt)
+        self.front = elt
+        return val
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_queue_usage(self):
+        queue = QueueTwoStacks()
+
+        queue.enqueue(1)
+        queue.enqueue(2)
+        queue.enqueue(3)
+
+        actual = queue.dequeue()
+        expected = 1
+        self.assertEqual(actual, expected)
+
+        actual = queue.dequeue()
+        expected = 2
+        self.assertEqual(actual, expected)
+
+        queue.enqueue(4)
+
+        actual = queue.dequeue()
+        expected = 3
+        self.assertEqual(actual, expected)
+
+        actual = queue.dequeue()
+        expected = 4
+        self.assertEqual(actual, expected)
+
+        with self.assertRaises(Exception):
+            queue.dequeue()
+
+
+unittest.main(verbosity=2)
---
+++ week1/day6/largestInStack.java
@@ -0,0 +1,79 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import static org.junit.Assert.*;
+import java.util.*;
+
+public class Solution {
+
+    // fill in the definitions for push(), pop(), and getMax()
+    
+
+    public static class MaxStack {
+
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> maxesStack = new Stack<>();
+
+    // Add a new item to the top of our stack. If the item is greater
+    // than or equal to the last item in maxesStack, it's
+    // the new max! So we'll add it to maxesStack.
+    public void push(int item) {
+        stack.push(item);
+        if (maxesStack.empty() || item >= maxesStack.peek()) {
+            maxesStack.push(item);
+        }
+    }
+
+    // Remove and return the top item from our stack. If it equals
+    // the top item in maxesStack, they must have been pushed in together.
+    // So we'll pop it out of maxesStack too.
+    public int pop() {
+        int item = stack.pop();
+        if (item == maxesStack.peek()) {
+            maxesStack.pop();
+        }
+        return item;
+    }
+
+    // The last item in maxesStack is the max item in our stack.
+    public int getMax() {
+        return maxesStack.peek();
+    }
+}
+
+
+
+    // tests
+
+    @Test
+    public void maxStackTest() {
+        final MaxStack s = new MaxStack();
+        s.push(5);
+        assertEquals("check max after 1st push", 5, s.getMax());
+        s.push(4);
+        s.push(7);
+        s.push(7);
+        s.push(8);
+        assertEquals("check before 1st pop", 8, s.getMax());
+        assertEquals("check pop #1", 8, s.pop());
+        assertEquals("check max after 1st pop", 7, s.getMax());
+        assertEquals("check pop #2", 7, s.pop());
+        assertEquals("check max after 2nd pop", 7, s.getMax());
+        assertEquals("check pop #3", 7, s.pop());
+        assertEquals("check max after 3rd pop", 5, s.getMax());
+        assertEquals("check pop #4", 4, s.pop());
+        assertEquals("check max after 4th pop", 5, s.getMax());
+    }
+
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(Solution.class);
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed.");
+        }
+    }
+}
